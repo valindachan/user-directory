464b37a1564c1140db49c88e9e80b44200b67f01
Valinda Chan <valinda.chan@gmail.com>
2017-07-25 23:28:17 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const path = require("path")
+const mustacheExpress = require("mustache-express")
+const data = require("./data")
+
+const app = express()
+app.use(express.static("public"))
+
+app.engine("mustache", mustacheExpress())
+app.set("views", "./views")
+app.set("view engine", "mustache")
+
+app.get("/", (req, res) => {
+  res.render("home", data)
+})
+
+app.use(express.static("public"))
+app.get("/profile/:id", (req, res) => {
+  const userDetails = data.users[req.params.id - 1]
+  // console.log(userDetails)
+  res.render("profile", userDetails)
+})
+
+app.listen(3000, function() {
+  console.log("Successfully started User Directory")
+})
